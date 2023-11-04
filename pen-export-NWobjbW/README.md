2391a0a41887b0f0567d702740d9bf3fcf7d713b
Gopalji-tech <149872742+Gopalji-tech@users.noreply.github.com>
2023-11-04 03:48:58 -0700

    Add files via upload

---
+++ pen-export-NWobjbW/README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Mantu-Sharma/pen/NWobjbW](https://codepen.io/Mantu-Sharma/pen/NWobjbW).
+
---
+++ pen-export-NWobjbW/dist/script.js
@@ -0,0 +1,14 @@
+const loveForm = document.getElementById('loveForm');
+const yourName = document.getElementById('yourName');
+const partnerName = document.getElementById('partnerName');
+const result = document.getElementById('result');
+const loveRate = document.getElementById('loveRate');
+const percentage = document.getElementById('percentage');
+
+loveForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const randomLoveRate = Math.floor(Math.random() * 101);
+    percentage.textContent = `${randomLoveRate}%`;
+    result.classList.remove('hidden');
+});
